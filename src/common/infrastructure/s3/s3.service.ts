f7ef4df2e4ac3f48258496f3d0bf1c96db1bfb28
Gabriel Nardone <gabriel.f.nardone@gmail.com>
2024-01-10 14:56:49 -0300

    Implements AWS S3
    Install aws sdk
    
    create a common uploadFile service to share between  modules

---
+++ src/common/infrastructure/s3/s3.service.ts
@@ -0,0 +1,26 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { v4 as uuid } from 'uuid';
+
+import { IFileUploadService } from '@/common/application/repository/file-upload.interface.repository';
+
+export class S3Service implements IFileUploadService {
+  constructor(
+    private readonly s3 = new S3Client({ region: process.env.AWS_S3_REGION }),
+  ) {}
+
+  async uploadFiles(file: Buffer): Promise<string> {
+    const fileKey = uuid();
+
+    await this.s3.send(
+      new PutObjectCommand({
+        Bucket: process.env.AWS_S3_BUCKET,
+        Key: fileKey,
+        Body: file,
+      }),
+    );
+
+    const objectPath = `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_S3_REGION}.amazonaws.com/${fileKey}`;
+
+    return objectPath;
+  }
+}
