ede65472aea417003d2d254821ef74cc9927e62a
GabrielNardone <gabriel.f.nardone@gmail.com>
2024-01-24 13:42:42 -0300

    Refactor s3 service
    Pass the needed properties to S3Client
    
    Set variables through ConfigService

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -2,4 +2,11 @@ export const configuration = () => ({
   server: {
     port: Number(process.env.PORT),
   },
+  s3: {
+    accesskey: process.env.AWS_S3_ACCESS_KEY,
+    secretKey: process.env.AWS_S3_SECRET_KEY,
+    bucket: process.env.AWS_S3_BUCKET,
+    region: process.env.AWS_S3_REGION,
+    endpoint: process.env.AWS_S3_ENDPOINT,
+  },
 });
