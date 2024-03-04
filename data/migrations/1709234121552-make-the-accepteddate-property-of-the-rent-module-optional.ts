a35889b2bcf56e2b560a2dc52ab9824f276db6e4
GabrielNardone <gabriel.f.nardone@gmail.com>
2024-03-04 14:00:26 -0300

    change last migration name

--- data/migrations/1709234121552-make-the-accepteddate-property-of-the-rent-module-optional.ts
+++ data/migrations/1709234121552-make-the-accepteddate-property-of-the-rent-module-optional.ts
@@ -1,9 +1,9 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class makeTheAccepteddatePropertyOfTheRentModuleOptional1709234121552
+export class updateAcceptedDateColumn1709234121552
   implements MigrationInterface
 {
-  name = 'makeTheAccepteddatePropertyOfTheRentModuleOptional1709234121552';
+  name = 'updateAcceptedDateColumn1709234121552';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
